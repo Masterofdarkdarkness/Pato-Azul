23b16750bd35d6206d9b631e7b690a158bf495db
Gabriel A. <142544309+Masterofdarkdarkness@users.noreply.github.com>
2024-09-10 20:33:29 -0300

    Create Consulta.java

---
+++ Consulta.java
@@ -0,0 +1,16 @@
+public class Consulta{
+    String nome;
+    
+    Consulta(String nome){
+        this.nome;
+    }
+    
+    String getNome(){
+        this.nome;
+    }
+    
+    void setNome(String nome){
+        return nome;
+    }
+    
+}
