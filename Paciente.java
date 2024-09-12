d583081a25659d44206fb17baf836b0fbbbf6655
Gabriel A. <142544309+Masterofdarkdarkness@users.noreply.github.com>
2024-09-12 16:15:24 -0300

    Create Paciente.java

---
+++ Paciente.java
@@ -0,0 +1,17 @@
+public class Paciente{
+    String nome;
+    int codigo;
+    
+    Paciente(String nome, int codigo){
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+    
+    String getNome(){
+        this.nome = nome;
+    }
+    
+    void setNome(String nome){
+        return nome;
+    }
+}
